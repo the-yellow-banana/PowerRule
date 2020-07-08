ce1c42913b6d72d67723dbd876ce848dc40f91d0
the-yellow-banana <67622759+the-yellow-banana@users.noreply.github.com>
2020-07-08 00:31:19 -0400

    Update POWER_RULE.py

--- POWER_RULE.py
+++ POWER_RULE.py
@@ -1,5 +1,4 @@
 equ = (input("enter your equation to find the derivative:")).lower()
-p=[]
 s=""
 def find_derivative (equ):
     terms = list(equ.split("+"))
@@ -8,14 +7,13 @@ def find_derivative (equ):
             if "^" in term:
                 poop = term.split("^")
                 power = poop[1]
-                new_power = int(power)-1
+                new_power = float(power)-1
                 coef = poop[0].split("x", -1)[0]
-                coef = int(s.join(coef))
-                coef = coef*(int(power))
+                coef = float(s.join(coef))
+                coef = coef*(float(power))
                 print(str(coef) + "x^" + str(new_power))
             elif "^" not in term:
                 print(term.split("x",1)[0])
 
 
-        #print(str(coef) + "x^" + str(new_power))
 find_derivative(equ)
