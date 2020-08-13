dae58917ada17108d93bd7e929db97913e8e95e7
the-yellow-banana <67622759+the-yellow-banana@users.noreply.github.com>
2020-08-13 18:33:12 -0400

    Update POWER_RULE.py

--- POWER_RULE.py
+++ POWER_RULE.py
@@ -1,8 +1,17 @@
-equ = (input("enter your equation to find the derivative:")).lower()
+##import needed libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+#empty string and list used to be added to further into the program
 s=""
 d=[]
-def find_derivative (equ):
+##derivative function only usefull for polynomials to de derived
+def find_derivative ():
+    ##get user input
+    global equ
+    equ = (input("enter your equation to find the derivative:")).lower()
     terms = list(equ.split("+"))
+    ##logic for power rule
     for term in terms:
         if "x" in term:
             if "^" in term:
@@ -16,5 +25,39 @@ def find_derivative (equ):
             elif "^" not in term:
                 d.append(term.split("x", 1)[0])
     print(d)
+find_derivative()
 
-find_derivative(equ)
+##make input equations graphable
+new_d = [w.replace('^', '**') for w in d]
+new_d = [w.replace('x', '*x') for w in new_d]
+new_d = ("+".join(new_d))
+
+new_equ = [w.replace('^', '**') for w in equ]
+new_equ = [w.replace('x', '*x') for w in new_equ]
+new_equ = ("".join(new_equ))
+
+#plot the derivative
+
+x = np.linspace(-5,5,100)
+y = eval(new_d)
+
+old_y= eval(new_equ)
+
+##plot jargin to make it look pretty
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+plt.grid()
+
+plt.plot(x,y, 'r', label= 'Derivative')
+plt.plot(x,old_y, 'b', label=' Original Equation')
+plt.legend(loc='upper left')
+
+# show the plot
+plt.show()
